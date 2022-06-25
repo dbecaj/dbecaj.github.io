a03ab5327abc30c88140e79c07a1a1ee94f95d73
dbecaj <dominik.becaj@gmail.com>
2022-06-25 21:24:48 +0200

    Replaced language text with buttons

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,14 @@
 import { Link } from "gatsby"
 import React from "react"
 import InfoItem from "./info-item"
+import { useLocation } from '@reach/router';
 
 export function Header(props) {
+  const location = useLocation()
+  const url = location.href ? location.href : '' 
+  let currLang = url.split('/').at(-1)
+  if (currLang === '') currLang = 'en'
+
   return (
     <div class={`flex flex-col w-full ${props.class}`}>
       <div class="flex ml-auto mt-4 mr-4 px-1.5 py-1 bg-gray-100 font-bold rounded-lg">
@@ -15,11 +21,18 @@ export function Header(props) {
             backgroundColor: "#374151",
             borderRadius: "50%"
           }
+          const langStyle = currLang === lang ? {
+            backgroundColor: "#374151",
+            color: "#f3f4f6",
+            borderRadius: "25%",
+            width: 30,
+            textAlign: "center"
+          } : { width: 30, textAlign: "center" }
 
           return (
             <div class="flex items-center text-gray-700">
-              {index > 0 ? <span class="mr-2 ml-2" style={circleStyle} /> : undefined}
-              <Link to={link}>{lang.toUpperCase()}</Link>
+              {index > 0 ? <span class="mr-1.5 ml-1.5" style={circleStyle} /> : undefined}
+              <Link to={link} style={langStyle}>{lang.toUpperCase()}</Link>
             </div>
           )
         })}
